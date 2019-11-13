d5ea2753c7b9a73cacfba08e8bc7e5f0d917ea53
lrodriguez22 <53873219+lrodriguez22@users.noreply.github.com>
2019-11-13 17:34:30 -0500

    clases del grafo

--- src/model/logic/MVCModelo.java
+++ src/model/logic/MVCModelo.java
@@ -1,6 +1,11 @@
 package model.logic;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 import model.data_structures.ArregloDinamico;
+import model.data_structures.Graph;
 import model.data_structures.IArregloDinamico;
 
 /**
@@ -28,6 +33,29 @@ public class MVCModelo {
 	public MVCModelo(int capacidad)
 	{
 		datos = new ArregloDinamico(capacidad);
+	}
+	
+	public void cargarTxtGraph() throws IOException
+	{
+		// Abre el archivo utilizando un FileReader
+		FileReader reader = new FileReader( "./data/Nodes_of_red_vial-wgs84_shp.txt"  );
+		// Utiliza un BufferedReader para leer por líneas
+		BufferedReader lector = new BufferedReader( reader );   
+		// Lee línea por línea del archivo
+		String linea = lector.readLine( );
+		while(linea!=null)
+		{
+			// Parte la línea con cada coma
+			String[] partes = linea.split( "," );
+			Graph nodoAux= new Graph(partes[0], partes[1], partes[2]);
+			int key=Integer.parseInt(partes[0]);
+			nodos.put(key, nodoAux);
+			linea=lector.readLine();
+		}
+		// Cierra los lectores y devuelve el resultado
+		lector.close( );
+		reader.close( );
+		System.out.println("El numero actual de nodos cargados es: "+nodos.size());
 	}
 	
 	/**
